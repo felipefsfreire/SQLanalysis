e5eaec37a1d79a5b7b5c09d8ef8d0cd1040a2e60
felipefsfreire <felipe.freire1983@gmail.com>
2025-03-08 07:19:32 -0300

    Add new challenge

--- analysis/4customerAboveAvarege.sql
+++ analysis/4customerAboveAvarege.sql
@@ -10,3 +10,29 @@
 -- avg_spent (média geral de gasto de todos os clientes, para referência)
 -- Ordene os resultados do maior para o menor total_spent.
 ---------------------------------------------------------------------------------------------------
+WITH customerSpent AS (
+    SELECT
+        customer_id,
+        customer,
+        SUM(total_price) AS total_spent
+    FROM sales
+    WHERE STRFTIME('%Y', sales_date) = '2024'
+    GROUP BY
+        customer_id,
+        customer
+),
+avgSpent AS (
+    SELECT 
+        ROUND(AVG(total_spent),2) AS avg_spent 
+    FROM customerSpent
+)
+
+SELECT
+    cs.customer_id,
+    cs.customer,
+    cs.total_spent,
+    a.avg_spent
+FROM customerSpent cs
+JOIN avgSpent a
+    ON cs.total_spent > a.avg_spent
+ORDER BY cs.total_spent DESC
